308f3ad59490687367077bf4dd98ada911dffcee
ElvantheG <elvangranth@gmail.com>
2026-04-12 11:17:40 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soul-leveling-v21';
+const CACHE_NAME = 'soul-leveling-v29';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
